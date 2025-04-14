0c809a97a57db60e50c46ea4cc90ef3dad11aa5a
Carolina da Silva Sousa <20221120770053@ifto.local>
2025-04-14 20:32:56 -0300

    Initial commit

---
+++ NetBeansProjects/PPCDesktop/src/main/java/br/edu/ifto/ppc/desktop/PPCDesktop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifto.ppc.desktop;
+
+/**
+ *
+ * @author 20221120770053
+ */
+public class PPCDesktop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
