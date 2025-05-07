34689351bd4bdd44b2744fe61cc965440ee2030a
Usuário <carolinadasilvasousa31@gmail.com>
2025-05-07 18:54:53 -0300

    a

---
+++ Sistem_Comunic/src/main/java/com/mycompany/sistem_comunic/Sistem_Comunic.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistem_comunic;
+
+/**
+ *
+ * @author Usuário
+ */
+public class Sistem_Comunic {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
